890fd1cb74493cb399702cdb2890f69c051d5e05
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-11 23:06:07 +0300

    Initial Commit

---
+++ SwiftUINotifiApp/SwiftUINotifiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINotifiAppApp.swift
+//  SwiftUINotifiApp
+//
+//  Created by rico on 11.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINotifiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
